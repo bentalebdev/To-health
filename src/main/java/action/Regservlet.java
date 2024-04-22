57ae08172c4c4e09e46099c2e1eba6af8da10d72
achraf-77733 <achrafbouaouich@gmail.com>
2024-04-22 15:09:43 +0000

    achraf

--- src/main/java/action/Regservlet.java
+++ src/main/java/action/Regservlet.java
@@ -12,11 +12,12 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-@WebServlet(value = {"/reg", "/accueil", "/rendezvous" })
+@WebServlet(value = {"/reg", "/accueil", "/rendezvous", "/login" })
 public class Regservlet extends HttpServlet {
 
 private final String index ="WEB-INF/html/index.html";
 private final String create = "WEB-INF/html/form.jsp";
+private final String login = "WEB-INF/html/logindoc.html";
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
@@ -33,10 +34,13 @@ private final String create = "WEB-INF/html/form.jsp";
                 case "/rendezvous":
                     request.getRequestDispatcher(create).forward(request, response);
                     break;
+                case "/login":
+                    request.getRequestDispatcher(login).forward(request,response);
 
                 default:
                     response.sendError(HttpServletResponse.SC_NOT_FOUND, "Resource not found.");
                     break;
+
             }
         } catch (ServletException | IOException e) {
             throw new RuntimeException(e);
