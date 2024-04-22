91a9e490cbbf7f96f04bf38bf1212d9a68fd3025
soufgame <nhailasoufiane2003@gmail.com>
2024-04-22 17:01:06 +0100

    soufiane

--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -9,8 +9,8 @@ public class DB {
         if(con==null)
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/anpe_db";
-            con = DriverManager.getConnection(url,"root","");
+            String url = "jdbc:mysql://localhost:3306/To_health";
+            con = DriverManager.getConnection(url,"root","Soufiane@2003");
         }
         return con;
     }
